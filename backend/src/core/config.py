ac3de1b0c3b2084977cadcd4bbf114bc4dfd546c
QuickFox43 <koalal1428@gmail.com>
2025-04-19 11:35:34 +0300

    feat(config): add project settings

---
+++ backend/src/core/config.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+PATH = Path(__file__).parent.parent.parent
+
+
+class PostgresConfig(BaseModel):
+    user: str
+    password: str
+    host: str
+    port: int
+    db: str
+    echo: bool = False
+    naming_convention: dict = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+    @property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
+
+class JWTConfig(BaseModel):
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+    refresh_token_expire_days: int
+
+
+class RedisConfig(BaseModel):
+    host: str
+    port: int
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_file=PATH / ".env",
+        extra="ignore",
+    )
+
+    project_name: str = "Food Social Network"
+    postgres: PostgresConfig
+    jwt: JWTConfig
+    redis: RedisConfig
+    mode: Literal["dev", "test", "prod"] = Field(default="prod", description="Application mode")
+
+
+settings = Settings()
