ea64fdc527823c80a0c508acbe62e553668edbb9
QuickFox43 <koalal1428@gmail.com>
2025-04-19 11:55:54 +0300

    feat(api): add fastapi launch script

---
+++ backend/src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.core.config import settings
+
+app = FastAPI(
+    title=settings.project_name,
+    description="API for Food Social Network",
+    version="1.0.0",
+    debug=settings.debug,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.server.allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
