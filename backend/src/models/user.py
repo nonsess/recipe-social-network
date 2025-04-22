f7fe8985e349f79b1fac573677333eb99de9d8ab
QuickFox43 <koalal1428@gmail.com>
2025-04-22 18:15:28 +0300

    feat(models): add user model

---
+++ backend/src/models/user.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.models.base import Base
+
+if TYPE_CHECKING:
+    from src.models.token import RefreshToken
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+    is_active: Mapped[bool] = mapped_column(default=True)
+    is_superuser: Mapped[bool] = mapped_column(default=False)
+    last_login: Mapped[datetime | None] = mapped_column(default=None)
+
+    refresh_tokens: Mapped[list["RefreshToken"]] = relationship(back_populates="user")
