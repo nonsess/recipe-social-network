e10fb78fa2944c670167b183aef7ff2212d61396
QuickFox43 <koalal1428@gmail.com>
2025-04-24 19:37:07 +0300

    feat(services): add security service

---
+++ backend/src/services/security.py
@@ -0,0 +1,13 @@
+from passlib.context import CryptContext
+
+
+class SecurityService:
+    pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+    @classmethod
+    def verify_password(cls, plain_password: str, hashed_password: str) -> bool:
+        return cls.pwd_context.verify(plain_password, hashed_password)
+
+    @classmethod
+    def get_password_hash(cls, password: str) -> str:
+        return cls.pwd_context.hash(password)
