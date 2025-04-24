5cb47eec2e40af869407420bf58139d6d3e721c3
QuickFox43 <koalal1428@gmail.com>
2025-04-24 19:35:46 +0300

    feat(schemas): add token schema

---
+++ backend/src/schemas/token.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class TokenPayload(BaseModel):
+    sub: int
+    exp: datetime
