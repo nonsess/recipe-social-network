eb6317b4d2cb20c9af4ca9e2c4265c308f163fae
QuickFox43 <koalal1428@gmail.com>
2025-04-24 19:36:17 +0300

    refactor: add __init__.py for schemas

--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,3 +1,12 @@
 from src.schemas.base import BaseReadSchema, BaseSchema
+from src.schemas.token import Token, TokenPayload
+from src.schemas.user import UserCreate, UserRead
 
-__all__ = ["BaseReadSchema", "BaseSchema"]
+__all__ = [
+    "BaseReadSchema",
+    "BaseSchema",
+    "Token",
+    "TokenPayload",
+    "UserCreate",
+    "UserRead",
+]
