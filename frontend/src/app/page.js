3be37a3bd3ff21cda05e1769ed48dfe06fc760a9
Danil Petrov <petrovdanil230@gmail.com>
2025-04-27 00:11:00 +0800

    refactor(context): added context, removed useEffect

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,41 +1,26 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useRecipes } from "@/context/RecipeContext"
 import Container from "@/components/Container"
-import RecipeCard from "../components/shared/RecipeCard";
-
-import RecipesService from "../services/recipes.service";
+import RecipeCard from "../components/shared/RecipeCard"
+import Loader from "@/components/ui/Loader"
 
 export default function App() {
-  const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const { recipes, loading } = useRecipes()
 
-  useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const data = await RecipesService.getAllReceipts();
-        setRecipes(data);
-      } catch (error) {
-        console.error("Ошибка при загрузке рецептов:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+    if (loading) {
+        return (
+            <Loader />
+        )
+    }
 
-    fetchRecipes();
-  }, []);
-
-  if (loading) {
-    return <Container className="py-6">Загрузка...</Container>;
-  }
-
-  return (
-    <Container className={"py-6"}>
-        <h2 className="text-2xl font-bold mb-6">Популярные рецепты</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {recipes.map((recipe) => (
-            <RecipeCard key={recipe.id} recipe={recipe} />
-          ))}
-        </div>
-    </Container>
-  )
+    return (
+        <Container className="py-6 flex-1">
+            <h2 className="text-2xl font-bold mb-6">Популярные рецепты</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {recipes.map((recipe) => (
+                    <RecipeCard key={recipe.id} recipe={recipe} />
+                ))}
+            </div>
+        </Container>
+    )
 }
