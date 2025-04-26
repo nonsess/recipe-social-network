3ab1a38affd23a65f3cd28e4a9264d37e3f60b5a
Danil Petrov <petrovdanil230@gmail.com>
2025-04-27 00:10:10 +0800

    feat(context providers): added context providers

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,24 +1,39 @@
 import "./globals.css";
-
+import {Montserrat} from "next/font/google";
+import { RecipeProvider } from "@/context/RecipeContext";
+import { UserProvider } from "@/context/UserContext";
 import Header from "@/components/Header";
 import DesktopSidebar from "@/components/DesktopSidebar";
 import MobileMenu from "@/components/MobileMenu";
-import Footer from "@/components/Footer";
+
+const montserrat = Montserrat({
+    variable: "--font-montserrat",
+    weight: ["400", "500", "600", "700"],
+    subsets: ["cyrillic"],
+})
+
+export const metadata = {
+    title: "Рецепты - Ваши любимые рецепты и кулинарные идеи",
+    description: "Найдите лучшие рецепты, советы по кулинарии и идеи для приготовления блюд.",
+};
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="ru">
             <head />
-            <body className="min-h-screen">
-              <Header />
-              <div className="flex flex-1">
-                  <DesktopSidebar />
-                  <main className="flex-1 md:pl-64 pt-12 bg-[#F5F2F2]">
-                      {children}
-                      <Footer />
-                  </main>
-              </div>
-              <MobileMenu />
+            <body className="min-h-screen bg-gradient-to-b from-background to-secondary/100">
+                <RecipeProvider>
+                    <UserProvider>
+                        <Header />
+                        <div className="flex flex-1">
+                            <DesktopSidebar />
+                            <main className="flex-1 md:pl-64 pt-12 bg-[#F5F2F2] h-full">
+                                {children}
+                            </main>
+                        </div>
+                        <MobileMenu />
+                    </UserProvider>
+                </RecipeProvider>
             </body>
         </html>
     );
