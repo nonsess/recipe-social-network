e0d6f959a75fc85a5d5f0e9b4c7c93fca51e9a01
Danil Petrov <petrovdanil230@gmail.com>
2025-04-27 00:11:31 +0800

    feat(login|reg): added login|reg pages

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import Container from "@/components/Container";
+
+export default function LoginPage() {
+    return (
+        <Container className="py-8 h-screen">
+           Вход в аккаунт
+        </Container>
+    );
+};
---
+++ frontend/src/app/registration/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import Container from "@/components/Container";
+
+export default function RegistrationPage() {
+    return (
+        <Container className="py-8 h-screen">
+           Создание аккаунта
+        </Container>
+    );
+};
