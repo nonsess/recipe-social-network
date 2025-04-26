03c9fdcf5def568562fe86852d84a56297a1a90a
Danil Petrov <petrovdanil230@gmail.com>
2025-04-27 00:12:40 +0800

    feat(recipe add page): added recipe add page

---
+++ frontend/src/app/recipe/add/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import Container from "@/components/Container";
+
+export default function AddRecipePage() {
+    return (
+        <Container className="py-8 h-screen">
+           Добавление рецепта
+        </Container>
+    );
+};
