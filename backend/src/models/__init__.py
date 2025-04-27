1a7547bcc9720ab2b21b375497b1e64e4c7e3bd7
QuickFox43 <koalal1428@gmail.com>
2025-04-27 19:41:53 +0300

    fix(models): import user profile from user_profile instead of user

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,5 +1,6 @@
 from src.models.base import Base
 from src.models.token import RefreshToken
 from src.models.user import User
+from src.models.user_profile import UserProfile
 
-__all__ = ["Base", "RefreshToken", "User"]
+__all__ = ["Base", "RefreshToken", "User", "UserProfile"]
