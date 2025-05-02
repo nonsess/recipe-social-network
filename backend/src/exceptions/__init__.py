02b8e428dc4965f6949820df599df0a43648105e
QuickFox43 <koalal1428@gmail.com>
2025-05-02 16:41:14 +0300

    feat(exceptions): add user exceptions

--- backend/src/exceptions/__init__.py
+++ backend/src/exceptions/__init__.py
@@ -1,7 +1,11 @@
 from src.exceptions.base import BaseAppError
 from src.exceptions.http import AppHTTPException
+from src.exceptions.user import UserEmailAlreadyExistsError, UserNicknameAlreadyExistsError, UserNotFoundError
 
 __all__ = [
     "AppHTTPException",
     "BaseAppError",
+    "UserEmailAlreadyExistsError",
+    "UserNicknameAlreadyExistsError",
+    "UserNotFoundError",
 ]
---
+++ backend/src/exceptions/user.py
@@ -0,0 +1,13 @@
+from src.exceptions.base import BaseAppError
+
+
+class UserNotFoundError(BaseAppError):
+    error_key = "user_not_found"
+
+
+class UserNicknameAlreadyExistsError(BaseAppError):
+    error_key = "user_nickname_already_exists"
+
+
+class UserEmailAlreadyExistsError(BaseAppError):
+    error_key = "user_email_already_exists"
