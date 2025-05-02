85200a840993c8ebd0f169b52b1759231502fe2e
Danil Petrov <petrovdanil230@gmail.com>
2025-05-03 01:21:29 +0800

    feat(components): added new component AuthorCard

---
+++ frontend/src/components/shared/AuthorCard.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function AuthorCard({ author }) {
+    return (
+        <Link href={`/profile/${author.id}`}>
+            <div className="flex rounded-xl bg-card p-4 cursor-pointer items-center mb-6">
+                <div className="relative w-12 h-12 rounded-full overflow-hidden mr-4">
+                    <Image
+                        src={author.avatar}
+                        alt={author.name}
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+                <div>
+                    <p className="text-sm text-gray-600">Автор рецепта</p>
+                    <p className="font-medium">{author.name}</p>
+                </div>
+            </div>
+        </Link>
+    )
+}
