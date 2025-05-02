266eba821e6fcbf1966389939204b8219ef1af26
QuickFox43 <koalal1428@gmail.com>
2025-05-02 19:22:01 +0300

    feat(exceptions): add image too large, wrong image format errors

--- backend/src/exceptions/__init__.py
+++ backend/src/exceptions/__init__.py
@@ -1,11 +1,14 @@
 from src.exceptions.base import BaseAppError
 from src.exceptions.http import AppHTTPException
+from src.exceptions.image import ImageTooLargeError, WrongImageFormatError
 from src.exceptions.user import UserEmailAlreadyExistsError, UserNicknameAlreadyExistsError, UserNotFoundError
 
 __all__ = [
     "AppHTTPException",
     "BaseAppError",
+    "ImageTooLargeError",
     "UserEmailAlreadyExistsError",
     "UserNicknameAlreadyExistsError",
     "UserNotFoundError",
+    "WrongImageFormatError",
 ]
