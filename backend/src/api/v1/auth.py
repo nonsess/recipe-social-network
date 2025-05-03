df81893007783c8cd9d733063efdd928d3a347c8
QuickFox43 <koalal1428@gmail.com>
2025-05-03 23:14:28 +0300

    docs(api): add docs for responses to auth endpoints

--- backend/src/api/v1/auth.py
+++ backend/src/api/v1/auth.py
@@ -15,11 +15,38 @@ from src.schemas.token import Token
 from src.schemas.user import UserCreate, UserLogin, UserRead
 from src.services import SecurityService, TokenService, UserService
 from src.services.token import RefreshTokenService
+from src.utils import json_example_factory, json_examples_factory
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/register", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/register",
+    summary="Register a new user",
+    description="Register a new user with a username, email, and password",
+    response_model=UserRead,
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_409_CONFLICT: {
+            "content": json_examples_factory(
+                {
+                    "User nickname already exists": {
+                        "value": {
+                            "detail": "Username already taken",
+                            "error_key": "user_nickname_already_exists",
+                        },
+                    },
+                    "User email already exists": {
+                        "value": {
+                            "detail": "Email already registered",
+                            "error_key": "user_email_already_exists",
+                        },
+                    },
+                },
+            ),
+        },
+    },
+)
 async def register(
     user_in: UserCreate,
     session: SessionDependency,
@@ -47,7 +74,32 @@ async def register(
     return user
 
 
-@router.post("/login")
+@router.post(
+    "/login",
+    summary="Login a user",
+    description="Login a user with an email or username and password",
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Incorrect email/username or password",
+            "content": json_examples_factory(
+                {
+                    "Incorrect email/username or password": {
+                        "value": {
+                            "detail": "Incorrect email/username or password",
+                            "error_key": "incorrect_email_username_or_password",
+                        },
+                    },
+                    "Inactive user": {
+                        "value": {
+                            "detail": "Inactive user",
+                            "error_key": "inactive_user",
+                        },
+                    },
+                },
+            ),
+        },
+    },
+)
 async def login(
     user_in: UserLogin,
     session: SessionDependency,
@@ -117,7 +169,21 @@ async def login(
     )
 
 
-@router.post("/refresh")
+@router.post(
+    "/refresh",
+    summary="Refresh a user's access token",
+    description="Refresh a user's access token with a refresh token",
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "content": json_example_factory(
+                {
+                    "detail": "Invalid refresh token",
+                    "error_key": "invalid_refresh_token",
+                },
+            ),
+        },
+    },
+)
 async def refresh_token(
     refresh_token: str,
     session: SessionDependency,
