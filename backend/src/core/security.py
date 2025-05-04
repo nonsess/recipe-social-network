c3ea7f978f5097f404cfbda34756f2dd3b033093
QuickFox43 <koalal1428@gmail.com>
2025-05-04 14:54:55 +0300

    feat(services): add passing exceptions in auth business logic

--- backend/src/core/security.py
+++ backend/src/core/security.py
@@ -1,9 +1,16 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from src.dependencies import RedisDependency, SessionDependency
+from src.exceptions import (
+    AppHTTPException,
+    InactiveOrNotExistingUserError,
+    InvalidJWTError,
+    InvalidTokenError,
+    JWTSignatureExpiredError,
+)
 from src.models.user import User
 from src.services.token import TokenService
 
@@ -16,9 +23,38 @@ async def get_current_user(
     redis: RedisDependency,
 ) -> User:
     token_service = TokenService(session=session, redis=redis)
-    return await token_service.get_current_user(
-        token=credentials.credentials if credentials else None,
-    )
+    try:
+        return await token_service.get_current_user(
+            token=credentials.credentials if credentials else None,
+        )
+    except InvalidTokenError as e:
+        raise AppHTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=e.message,
+            error_key=e.error_key,
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from e
+    except InactiveOrNotExistingUserError as e:
+        raise AppHTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=e.message,
+            error_key=e.error_key,
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from None
+    except InvalidJWTError as e:
+        raise AppHTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=e.message,
+            error_key=e.error_key,
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from None
+    except JWTSignatureExpiredError as e:
+        raise AppHTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=e.message,
+            error_key=e.error_key,
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from None
 
 
 CurrentUserDependency = Annotated[User, Depends(get_current_user)]
--- backend/src/services/token.py
+++ backend/src/services/token.py
@@ -1,12 +1,17 @@
 from datetime import UTC, datetime, timedelta
 
-from fastapi import HTTPException, status
-from jose import JWTError, jwt
+from jose import ExpiredSignatureError, JWTError, jwt
 from redis.asyncio import Redis
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.config import settings
+from src.exceptions.auth import (
+    InactiveOrNotExistingUserError,
+    InvalidJWTError,
+    InvalidTokenError,
+    JWTSignatureExpiredError,
+)
 from src.models.token import RefreshToken
 from src.models.user import User
 
@@ -15,13 +20,6 @@ class TokenService:
     def __init__(self, session: AsyncSession, redis: Redis) -> None:
         self.session = session
         self.redis = redis
-
-    def _exception_fabric(self, detail: str) -> HTTPException:
-        return HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=detail,
-            headers={"WWW-Authenticate": "Bearer"},
-        )
 
     def create_access_token(self, data: dict, expires_delta: timedelta | None = None) -> str:
         to_encode = data.copy()
@@ -39,26 +37,29 @@ class TokenService:
                 settings.jwt.secret_key,
                 algorithms=settings.jwt.algorithm,
             )
-        except JWTError as e:
-            msg = f"Could not validate credentials: {e}"
-            raise self._exception_fabric(msg) from None
+        except ExpiredSignatureError:
+            msg = "Could not validate credentials: token has expired"
+            raise JWTSignatureExpiredError(msg) from None
+        except JWTError:
+            msg = "Could not validate credentials: invalid jwt"
+            raise InvalidJWTError(msg) from None
 
     async def get_current_user(self, token: str | None) -> User:
         if not token:
-            msg = "Could not validate credentials"
-            raise self._exception_fabric(msg)
+            msg = "Could not validate credentials: no scheme or token in Authorization header"
+            raise InvalidTokenError(msg)
 
         payload = await self.verify_token(token)
         user_id_str: str | None = payload.get("sub")
         if user_id_str is None:
-            msg = "Could not validate credentials: no user ID in JWT"
-            raise self._exception_fabric(msg)
+            msg = "Could not validate credentials: no user ID in payload"
+            raise InvalidTokenError(msg)
 
         user_id = int(user_id_str)
         user = await self.session.scalar(select(User).where(User.id == user_id))
         if not user or not user.is_active:
             msg = "Could not validate credentials: user is inactive or user does not exists"
-            raise self._exception_fabric(msg)
+            raise InactiveOrNotExistingUserError(msg)
 
         return user
 
@@ -66,7 +67,7 @@ class TokenService:
         token_in_redis = await self.redis.get(f"refresh_token:{token}")
         if not token_in_redis:
             msg = "Invalid refresh token"
-            raise self._exception_fabric(msg)
+            raise InvalidTokenError(msg)
 
         refresh_token = await self.session.scalar(
             select(RefreshToken).where(
@@ -77,7 +78,7 @@ class TokenService:
         )
         if not refresh_token:
             msg = "Invalid refresh token"
-            raise self._exception_fabric(msg)
+            raise InvalidTokenError(msg)
 
         return refresh_token
 
