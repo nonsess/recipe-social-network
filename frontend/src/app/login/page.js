e7788b98f3bfe863f1a8406c2f41e3dcbf135061
Danil Petrov <petrovdanil230@gmail.com>
2025-05-04 19:51:21 +0800

    fix(path): fixed import path.

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Container from "@/components/Container";
+import Container from "@/components/layout/Container";
 
 export default function LoginPage() {
     return (
