4e1515f61d3c81ea27256749b912a0e38e21be18
Danil Petrov <petrovdanil230@gmail.com>
2025-05-04 19:50:33 +0800

    refactor(decomposition): decomposed the logic into another component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import { useRecipes } from "@/context/RecipeContext"
-import Container from "@/components/Container"
-import RecipeCard from "../components/shared/RecipeCard"
+import Container from "@/components/layout/Container"
 import Loader from "@/components/ui/Loader"
+import RecipesList from "@/components/layout/RecipesList";
 
 export default function App() {
     const { recipes, loading } = useRecipes()
@@ -16,11 +16,7 @@ export default function App() {
     return (
         <Container className="py-6 flex-1">
             <h2 className="text-2xl font-bold mb-6">Популярные рецепты</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {recipes.map((recipe) => (
-                    <RecipeCard key={recipe.id} recipe={recipe} />
-                ))}
-            </div>
+            <RecipesList recipes={recipes}/>
         </Container>
     )
 }
