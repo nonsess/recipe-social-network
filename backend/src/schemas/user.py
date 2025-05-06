8e3daddbf63de2b5fb8f88e13b2ab5477b904e8b
QuickFox43 <koalal1428@gmail.com>
2025-05-06 19:42:19 +0300

    feat(auth): add checking password for strength

--- backend/src/schemas/user.py
+++ backend/src/schemas/user.py
@@ -21,6 +21,8 @@ BANNED_USERNAMES = [
 
 NICKNAME_PATTERN: Final[re.Pattern] = re.compile(r"^[a-zA-Z0-9_-]+$")
 
+SPECIAL_CHARACTERS: Final[str] = r"!\"#\$%&'\(\)\*\+,\-./:;<=>\?@\[\\\]\^_`{\|}~"
+
 Nickname = Annotated[str, Field(pattern=NICKNAME_PATTERN, min_length=3, max_length=30)]
 
 
@@ -48,6 +50,22 @@ class UserCreate(BaseModel):
             msg = "Username is not allowed"
             raise ValueError(msg)
         return username
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, password: str) -> str:
+        if (
+            not any(char.isupper() for char in password)
+            or not any(char.islower() for char in password)
+            or not any(char.isnumeric() for char in password)
+            or not re.search(r"[" + SPECIAL_CHARACTERS + r"]", password)
+        ):
+            msg = (
+                "Password must contain at least one uppercase letter, "
+                "one lowercase letter, one special symbol and number"
+            )
+            raise ValueError(msg)
+        return password
 
 
 class UserRead(BaseReadSchema):
