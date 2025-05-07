2bae7893fce29206abc015e6eddf05d978831d27
QuickFox43 <koalal1428@gmail.com>
2025-05-07 18:00:58 +0300

    feat(schemas): add banned emails schemas
    Add banned email create and read schemas

--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,8 +1,11 @@
+from src.schemas.banned_email import BannedEmailDomainCreate, BannedEmailDomainRead
 from src.schemas.base import BaseReadSchema, BaseSchema
 from src.schemas.token import Token, TokenPayload
 from src.schemas.user import UserCreate, UserProfileRead, UserProfileUpdate, UserRead, UserUpdate
 
 __all__ = [
+    "BannedEmailDomainCreate",
+    "BannedEmailDomainRead",
     "BaseReadSchema",
     "BaseSchema",
     "Token",
