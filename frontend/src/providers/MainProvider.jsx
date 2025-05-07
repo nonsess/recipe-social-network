2dbe74317b6892988ab900c1906ce326e3c58a39
Danil Petrov <petrovdanil230@gmail.com>
2025-05-07 22:48:38 +0800

    feat(auth): added auth v1

--- frontend/src/providers/MainProvider.jsx
+++ frontend/src/providers/MainProvider.jsx
@@ -2,17 +2,20 @@ import { RecipeProvider } from "./RecipeProvider"
 import { UserProvider } from "./UserProvider"
 import { FavoritesProvider } from "./FavoritesProvider"
 import { SearchHistoryProvider } from "./SearchHistoryProvider"
+import { AuthProvider } from "@/context/AuthContext"
 
 export default function MainProvider({ children }) {
     return (
-        <RecipeProvider>
-            <UserProvider>
-                <FavoritesProvider>
-                    <SearchHistoryProvider>
-                        {children}
-                    </SearchHistoryProvider>
-                </FavoritesProvider>
-            </UserProvider>
-        </RecipeProvider>
+        <AuthProvider>
+            <RecipeProvider>
+                <UserProvider>
+                    <FavoritesProvider>
+                        <SearchHistoryProvider>
+                            {children}
+                        </SearchHistoryProvider>
+                    </FavoritesProvider>
+                </UserProvider>
+            </RecipeProvider>
+        </AuthProvider>
     )
 }
