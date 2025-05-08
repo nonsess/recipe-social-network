0c4d5badb69d91358a05e4b75a09ea5859089e28
Danil Petrov <petrovdanil230@gmail.com>
2025-05-09 00:05:25 +0800

    chore(config): add allowed images hostnames

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+            },
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
