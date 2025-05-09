be723df18853d94793dd7307b8e7fd54b665827e
Danil Petrov <petrovdanil230@gmail.com>
2025-05-09 17:43:53 +0800

    fix(component): move author card to ui components

--- frontend/src/components/ui/AuthorCard.jsx
+++ frontend/src/components/ui/AuthorCard.jsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function AuthorCard({ author }) {
     return (
         <Link href={`/profile/${author.id}`}>
-            <div className="flex rounded-xl bg-card p-4 cursor-pointer items-center mb-6">
+            <div className="flex rounded-lg bg-card m-4 p-4 cursor-pointer items-center mb-6">
                 <div className="relative w-12 h-12 rounded-full overflow-hidden mr-4">
                     <Image
                         src={author.avatar}
