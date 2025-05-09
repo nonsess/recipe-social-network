12e89096e2fb92660fc1a70e394262a58119e9e2
QuickFox43 <koalal1428@gmail.com>
2025-05-09 19:41:05 +0300

    feat(exceptions): add recipe ownership error

--- backend/src/exceptions/recipe.py
+++ backend/src/exceptions/recipe.py
@@ -3,3 +3,8 @@ from src.exceptions.base import BaseAppError
 
 class RecipeNotFoundError(BaseAppError):
     error_key = "recipe_not_found"
+
+
+
+class RecipeOwnershipError(BaseAppError):
+    error_key = "recipe_belongs_to_other_user"
