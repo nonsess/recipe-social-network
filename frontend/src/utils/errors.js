e03fbdd48d5e6f2da0bbe53557a9066ed0c86999
Danil Petrov <petrovdanil230@gmail.com>
2025-05-11 00:09:17 +0800

    refactor(errors): move errors

---
+++ frontend/src/utils/errors.js
@@ -0,0 +1,24 @@
+export class CustomError extends Error {
+    constructor(message, name = 'Error') {
+        super(message);
+        this.name = name;
+    }
+}
+
+export class NetworkError extends CustomError {
+    constructor(message) {
+        super(message, 'NetworkError');
+    }
+}
+
+export class AuthError extends CustomError {
+    constructor(message) {
+        super(message, 'AuthError');
+    }
+}
+
+export class ValidationError extends CustomError {
+    constructor(message) {
+        super(message, 'ValidationError');
+    }
+} 
