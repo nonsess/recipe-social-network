57071bf3250511f8f96aa121fc7765304bacf8c0
Danil Petrov <petrovdanil230@gmail.com>
2025-05-11 00:09:36 +0800

    lint(comments): del comments

--- frontend/src/utils/tokenManager.js
+++ frontend/src/utils/tokenManager.js
@@ -6,7 +6,6 @@ class TokenManager {
         this.requestQueue = []
     }
 
-    // Проверяет, нужно ли обновить токен
     shouldRefreshToken() {
         const token = AuthService.getAccessToken()
         if (!token) return false
@@ -14,14 +13,12 @@ class TokenManager {
         try {
             const tokenData = JSON.parse(atob(token.split('.')[1]))
             const expirationTime = tokenData.exp * 1000
-            // Обновляем токен за 5 минут до истечения
             return Date.now() >= expirationTime - 5 * 60 * 1000
         } catch {
             return false
         }
     }
 
-    // Обновляет токен и выполняет отложенные запросы
     async refreshToken() {
         if (this.refreshPromise) {
             return this.refreshPromise
@@ -29,13 +26,11 @@ class TokenManager {
 
         this.refreshPromise = AuthService.refreshToken()
             .then((result) => {
-                // Выполняем все отложенные запросы
                 this.requestQueue.forEach(request => request.resolve())
                 this.requestQueue = []
                 return result
             })
             .catch((error) => {
-                // В случае ошибки отклоняем все отложенные запросы
                 this.requestQueue.forEach(request => request.reject(error))
                 this.requestQueue = []
                 throw error
@@ -47,14 +42,12 @@ class TokenManager {
         return this.refreshPromise
     }
 
-    // Добавляет запрос в очередь
     enqueueRequest() {
         return new Promise((resolve, reject) => {
             this.requestQueue.push({ resolve, reject })
         })
     }
 
-    // Проверяет токен перед запросом
     async ensureValidToken() {
         if (this.shouldRefreshToken()) {
             if (this.refreshPromise) {
