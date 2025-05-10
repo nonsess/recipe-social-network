b7d865330abf268b71d23d33922bff8c1598b67c
Danil Petrov <petrovdanil230@gmail.com>
2025-05-10 23:55:19 +0800

    feat(style): delete add recipe button

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -5,11 +5,8 @@ import { useRouter } from 'next/navigation'
 import { useAuth } from '@/context/AuthContext'
 import { useFavorites } from '@/context/FavoritesContext'
 import { useRecipes } from '@/context/RecipeContext'
-import { Button } from '@/components/ui/button'
 import Container from '@/components/layout/Container'
 import { useToast } from '@/hooks/use-toast'
-import { Plus } from 'lucide-react'
-import Link from 'next/link'
 import EditableProfileInfo from '@/components/ui/profile/EditableProfileInfo'
 import EditableProfilePhoto from '@/components/ui/profile/EditableProfilePhoto'
 import ProfileTabs from '@/components/ui/profile/ProfileTabs'
@@ -82,13 +79,7 @@ export default function ProfilePage() {
         <Container className="py-8">
             <div className="space-y-8">
                 <div className="flex items-start justify-between">
-                    <h1 className="text-3xl font-bold">Ваш профиль</h1>
-                    <Button asChild>
-                        <Link href="/recipe/add">
-                            <Plus className="mr-2 h-4 w-4" />
-                            Добавить рецепт
-                        </Link>
-                    </Button>
+                    <h1 className="text-3xl font-bold tracking-tight">Ваш профиль</h1>
                 </div>
 
                 <div className="flex flex-col md:flex-row gap-8 items-start">
