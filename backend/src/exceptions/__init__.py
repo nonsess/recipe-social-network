2b2aba2a4d813c0b86eb12810e79cf034f57950f
QuickFox43 <koalal1428@gmail.com>
2025-05-10 17:08:55 +0300

    fix(imports): add attach step error to __init__.py

--- backend/src/exceptions/__init__.py
+++ backend/src/exceptions/__init__.py
@@ -9,7 +9,12 @@ from src.exceptions.banned_email import EmailDomainAlreadyBannedError
 from src.exceptions.base import BaseAppError
 from src.exceptions.http import AppHTTPException
 from src.exceptions.image import ImageTooLargeError, WrongImageFormatError
-from src.exceptions.recipe import NoRecipeImageError, RecipeNotFoundError, RecipeOwnershipError
+from src.exceptions.recipe import (
+    AttachInstructionStepError,
+    NoRecipeImageError,
+    RecipeNotFoundError,
+    RecipeOwnershipError,
+)
 from src.exceptions.user import UserEmailAlreadyExistsError, UserNicknameAlreadyExistsError, UserNotFoundError
 
 __all__ = [
