78b1ffaa3ff5675c4eb5790d4b7963e1b86baaf8
QuickFox43 <koalal1428@gmail.com>
2025-05-11 12:45:22 +0300

    feat(models): add favorite recipe model

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,5 +1,6 @@
 from src.models.banned_email import BannedEmail
 from src.models.base import Base
+from src.models.favorite_recipes import FavoriteRecipe
 from src.models.recipe import Recipe
 from src.models.recipe_ingredient import RecipeIngredient
 from src.models.recipe_instructions import RecipeInstruction
@@ -11,6 +12,7 @@ from src.models.user_profile import UserProfile
 __all__ = [
     "BannedEmail",
     "Base",
+    "FavoriteRecipe",
     "Recipe",
     "RecipeIngredient",
     "RecipeInstruction",
