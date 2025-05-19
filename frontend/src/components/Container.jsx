67edef119dff3efde5b042d21ade480bd273c850
Danil Petrov <petrovdanil230@gmail.com>
2025-05-19 17:33:34 +0800

    del(extra): delete extra files

--- frontend/src/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children, className = '' }) => {
-  return (
-    <div className={`container mx-auto px-4 ${className}`}>
-      {children}
-    </div>
-  );
-};
-
-export default Container; 
