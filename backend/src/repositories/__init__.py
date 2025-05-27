9b1700289405c0487883a1e41095c0276bfe50ad
QuickFox43 <koalal1428@gmail.com>
2025-05-27 10:23:00 +0300

    chore(imports): add new repositories to __all__
    Add recipe image and recipe search repositories to __all__ in init file

--- backend/src/repositories/__init__.py
+++ backend/src/repositories/__init__.py
@@ -2,8 +2,10 @@ from src.repositories.banned_email import BannedEmailRepository
 from src.repositories.disliked_recipe import DislikedRecipeRepository
 from src.repositories.favorite_recipe import FavoriteRecipeRepository
 from src.repositories.recipe import RecipeRepository
+from src.repositories.recipe_image import RecipeImageRepository
 from src.repositories.recipe_ingredient import RecipeIngredientRepository
 from src.repositories.recipe_instruction import RecipeInstructionRepository
+from src.repositories.recipe_search import RecipeSearchRepository
 from src.repositories.recipe_tag import RecipeTagRepository
 from src.repositories.token import RefreshTokenRepository
 from src.repositories.user import UserRepository
@@ -13,9 +15,11 @@ __all__ = [
     "BannedEmailRepository",
     "DislikedRecipeRepository",
     "FavoriteRecipeRepository",
+    "RecipeImageRepository",
     "RecipeIngredientRepository",
     "RecipeInstructionRepository",
     "RecipeRepository",
+    "RecipeSearchRepository",
     "RecipeTagRepository",
     "RefreshTokenRepository",
     "UserProfileRepository",
