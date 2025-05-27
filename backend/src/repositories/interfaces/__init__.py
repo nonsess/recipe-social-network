f05f90187738a7999678e7263658f26477471916
QuickFox43 <koalal1428@gmail.com>
2025-05-27 09:10:44 +0300

    chore(imports): add __init__.py for interfaces

---
+++ backend/src/repositories/interfaces/__init__.py
@@ -0,0 +1,33 @@
+from src.repositories.interfaces.anonymous_user import AnonymousUserRepositoryProtocol
+from src.repositories.interfaces.banned_email import BannedEmailRepositoryProtocol
+from src.repositories.interfaces.disliked_recipe import DislikedRecipeRepositoryProtocol
+from src.repositories.interfaces.favorite_recipe import FavoriteRecipeRepositoryProtocol
+from src.repositories.interfaces.recipe import RecipeRepositoryProtocol
+from src.repositories.interfaces.recipe_image import RecipeImageRepositoryProtocol
+from src.repositories.interfaces.recipe_impression import RecipeImpressionRepositoryProtocol
+from src.repositories.interfaces.recipe_ingredient import RecipeIngredientRepositoryProtocol
+from src.repositories.interfaces.recipe_instruction import RecipeInstructionRepositoryProtocol
+from src.repositories.interfaces.recipe_search import RecipeSearchRepositoryProtocol
+from src.repositories.interfaces.recipe_tag import RecipeTagRepositoryProtocol
+from src.repositories.interfaces.token import RefreshTokenRepositoryProtocol
+from src.repositories.interfaces.user import UserRepositoryProtocol
+from src.repositories.interfaces.user_avatar import UserAvatarRepositoryProtocol
+from src.repositories.interfaces.user_profile import UserProfileRepositoryProtocol
+
+__all__ = [
+    "AnonymousUserRepositoryProtocol",
+    "BannedEmailRepositoryProtocol",
+    "DislikedRecipeRepositoryProtocol",
+    "FavoriteRecipeRepositoryProtocol",
+    "RecipeImageRepositoryProtocol",
+    "RecipeImpressionRepositoryProtocol",
+    "RecipeIngredientRepositoryProtocol",
+    "RecipeInstructionRepositoryProtocol",
+    "RecipeRepositoryProtocol",
+    "RecipeSearchRepositoryProtocol",
+    "RecipeTagRepositoryProtocol",
+    "RefreshTokenRepositoryProtocol",
+    "UserAvatarRepositoryProtocol",
+    "UserProfileRepositoryProtocol",
+    "UserRepositoryProtocol",
+]
