6e23ad2f30589d1957d5a8ea86b0ae45d4612b3f
QuickFox43 <koalal1428@gmail.com>
2025-05-28 13:25:04 +0300

    feat(exceptions): add instruction belong error

--- backend/src/exceptions/recipe.py
+++ backend/src/exceptions/recipe.py
@@ -19,3 +19,7 @@ class NoRecipeInstructionsError(BaseAppError):
 
 class AttachInstructionStepError(BaseAppError):
     error_key = "attach_instruction_step"
+
+
+class InstructionNotBelongsToRecipeError(BaseAppError):
+    error_key = "instruction_not_belongs_to_recipe"
