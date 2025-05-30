c47cc254f632f3a02b23ee42a88479cb2a3213e3
QuickFox43 <koalal1428@gmail.com>
2025-05-31 02:26:09 +0300

    feat(config): add recs project settings

---
+++ recsys/src/core/config.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+PATH = Path(__file__).parent.parent.parent
+
+
+class QdrantConfig(BaseModel):
+    host: str
+    port: int
+
+
+class PostgresConfig(BaseModel):
+    user: str
+    password: str
+    host: str
+    port: int
+    db: str
+    echo: bool = False
+    naming_convention: dict = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+    @property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
+
+class GigachatConfig(BaseModel):
+    api_key: str
+
+
+class NatsConfig(BaseModel):
+    host: str
+    port: int
+
+
+class AsgiFastStreamConfig(BaseModel):
+    host: str
+    port: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="RECSYS__", env_file=PATH.parent / ".env", env_nested_delimiter="__")
+
+    gigachat: GigachatConfig
+    qdrant: QdrantConfig
+    postgres: PostgresConfig
+    asgi_faststream: AsgiFastStreamConfig
+    nats: NatsConfig
+
+
+settings = Settings()
