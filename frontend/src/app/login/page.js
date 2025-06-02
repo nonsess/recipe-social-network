197b4086403490020aacd80ad7981ad5c236ab63
Danil Petrov <petrovdanil230@gmail.com>
2025-06-02 18:21:55 +0800

    feat(policy): add privacy policy

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -6,22 +6,25 @@ import EggBackground from "@/components/ui/EggBackground";
 export default function LoginPage() {
   return (
     <>
-      <EggBackground />
-      <Container className="h-[calc(100vh-200px)] flex items-center justify-center relative">
-        <div className="w-full max-w-md space-y-6 bg-white/80 backdrop-blur-md p-8 rounded-2xl shadow-lg border border-gray-100">
-          <div className="space-y-2 text-center">
-            <h1 className="text-4xl font-bold text-gray-900">Вход</h1>
-            <p className="text-gray-600">Добро пожаловать в мир вкусных рецептов</p>
-          </div>
-          <LoginForm />
-          <p className="text-center text-sm text-gray-600">
-            Нет аккаунта?{" "}
-            <Link href="/registration" className="text-orange-500 hover:text-orange-600 font-medium hover:underline">
-              Зарегистрироваться
-            </Link>
-          </p>
-        </div>
-      </Container>
+        <EggBackground />
+        <Container className="h-screen flex items-center justify-center relative">
+            <div className="w-full max-w-md space-y-6 bg-white/80 backdrop-blur-md p-8 rounded-2xl shadow-lg border border-gray-100">
+            <div className="space-y-2 text-center">
+                <h1 className="text-4xl font-bold text-gray-900">Вход</h1>
+                <p className="text-gray-600">Добро пожаловать в мир вкусных рецептов</p>
+            </div>
+            <LoginForm />
+            <div className='text-center text-sm'>
+                Нажимая на кнопку, вы даете согласие на <a className="text-blue-600 hover:underline" href='/docs/policy'>обработку персональных данных</a>.
+            </div>
+            <p className="text-center text-sm text-gray-600">
+                Нет аккаунта?{" "}
+                <Link href="/registration" className="text-orange-500 hover:text-orange-600 font-medium hover:underline">
+                    Зарегистрироваться
+                </Link>
+            </p>
+            </div>
+        </Container>
     </>
   );
 }
