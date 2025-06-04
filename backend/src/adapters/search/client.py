fa8acc8a10c18eac41a86506f04ab388df5eb55c
QuickFox43 <koalal1428@gmail.com>
2025-06-04 15:56:47 +0300

    feat(adapters): remove elasticsearch instance

--- backend/src/adapters/search/client.py
+++
@@ -1,9 +0,0 @@
-from elasticsearch import AsyncElasticsearch
-
-from src.core.config import settings
-
-elastic_search_client = AsyncElasticsearch(
-    hosts=[f"{settings.elasticsearch.host}:{settings.elasticsearch.port}"],
-    basic_auth=(settings.elasticsearch.user, settings.elasticsearch.password),
-    verify_certs=False,
-)
