88b82fa219c774d54576de5cc71fb38bd01f0303
QuickFox43 <koalal1428@gmail.com>
2025-06-11 07:52:09 +0300

    fix(init): check for superuser existence in init

--- backend/src/init_superuser.py
+++ backend/src/init_superuser.py
@@ -3,6 +3,7 @@ import asyncio
 from src.core.config import Settings
 from src.core.di import container
 from src.db.uow import SQLAlchemyUnitOfWork
+from src.enums.user_role import UserRoleEnum
 from src.repositories.interfaces.user import UserRepositoryProtocol
 from src.services.security import SecurityService
 
@@ -10,14 +11,19 @@ from src.services.security import SecurityService
 async def main() -> None:
     async with container() as request_container:
         uow = await request_container.get(SQLAlchemyUnitOfWork)
-        user_repository = await request_container.get(UserRepositoryProtocol)
+        user_repository: UserRepositoryProtocol = await request_container.get(UserRepositoryProtocol)
         settings = await request_container.get(Settings)
         hashed_password = SecurityService.get_password_hash(settings.superuser.password)
         async with uow:
+            if await user_repository.get_by_email(settings.superuser.email) or await user_repository.get_by_username(
+                settings.superuser.username
+            ):
+                return
             await user_repository.create_superuser(
                 username=settings.superuser.username,
                 email=settings.superuser.email,
                 hashed_password=hashed_password,
+                role=UserRoleEnum.SUPERUSER,
             )
             await uow.commit()
 
